d5a03a6d595a8d00f82df488953715ab218a7845
jorgevilasboas <jorge.sergipe@gmail.com>
2017-11-21 16:26:43 -0300

    Adds Basic JavaScript Module - Part 01

---
+++ Basic JavaScript/004 - Initializing Variables with the Assignment Operator.js
@@ -0,0 +1,7 @@
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+
+// Define a variable a with var and initialize it to a value of 9.
+var a = 9;
---
+++ Basic JavaScript/038 - Nest one Array within Another Array.js
@@ -0,0 +1,11 @@
+// Example
+var ourArray = [
+    ["the universe", 42],
+    ["everything", 101010]
+];
+
+// Only change code below this line.
+var myArray = [
+    ["This is a string", 42],
+    ["Another String", 909090]
+];
