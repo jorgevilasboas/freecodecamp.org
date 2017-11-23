9749fa75c2b3a2dac5d8b9a4ce9d3f4207b6cd04
jorgevilasboas <jorge.sergipe@gmail.com>
2017-11-23 15:51:53 -0300

    Adds Basic JavaScript Module - Part 03

---
+++ Basic JavaScript/088 - Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+var myMusic = [{
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CS",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    // Add record here  
+    {
+        "artist": "Green Day",
+        "title": "American Idiot",
+        "release_year": 2004,
+        "formats": [
+            "CS",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    }
+];
---
+++ Basic JavaScript/099 - Generate Random Whole Numbers with JavaScript.js
@@ -0,0 +1,9 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+    // Only change code below this line.
+
+    // Generate number between zero and nine
+    return Math.floor(Math.random() * 10);
+}
