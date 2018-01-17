5761400295c6e9c83d36b151d19e2d4826d53e7c
jorgevilasboas <jorge.sergipe@gmail.com>
2018-01-17 16:28:31 -0300

    Adds more Intermediate Algorithm Scripting

--- 001 - Front End Development Certification/006 - Object Oriented and Functional Programming/014 - Spinal Tap Case.js
+++
@@ -1,8 +0,0 @@
-function spinalCase(str) {
-    // "It's such a fine line between stupid, and clever."
-    // --David St. Hubbins    
-    return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase();   
-  }
-  
-  console.log(spinalCase('This Is Spinal Tap'));
-  
